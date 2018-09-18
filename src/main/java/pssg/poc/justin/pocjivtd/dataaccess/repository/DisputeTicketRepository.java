4e190eda3dbe3751301123930db563f2c28545b5
Echo Liang <echo.2.liang@gov.bc.ca>
2018-09-18 13:58:37 -0700

    fixed DisputeTicketRepository

--- src/main/java/pssg/poc/justin/pocjivtd/dataaccess/repository/DisputeTicketRepository.java
+++ src/main/java/pssg/poc/justin/pocjivtd/dataaccess/repository/DisputeTicketRepository.java
@@ -1,9 +1,27 @@
 package pssg.poc.justin.pocjivtd.dataaccess.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import pssg.poc.justin.pocjivtd.dataaccess.entity.DisputeTicketEntity;
 
+/**
+ * The Interface DisputeTicketRepository.
+ * @author HLiang
+ */
 public interface DisputeTicketRepository extends JpaRepository<DisputeTicketEntity, Long> {
-
+	
+	/**
+	 * Find by dispute id.
+	 *
+	 * @param disputeID the dispute ID
+	 * @return the optional
+	 */
+	Optional<DisputeTicketEntity> findByDisputeId(Long disputeID);
+	
+	/* (non-Javadoc)
+	 * @see org.springframework.data.repository.CrudRepository#save(S)
+	 */
+	DisputeTicketEntity save(DisputeTicketEntity affiliationEntity);
 }
