e8e2afda958f46b9d1da5552c01c5f96b5eebb84
Echo Liang <echo.2.liang@gov.bc.ca>
2018-09-18 13:11:05 -0700

    exclude DataSourceAutoConfiguration

--- src/main/java/pssg/poc/justin/pocjivtd/PocjivtdApplication.java
+++ src/main/java/pssg/poc/justin/pocjivtd/PocjivtdApplication.java
@@ -2,11 +2,12 @@ package pssg.poc.justin.pocjivtd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class PocjivtdApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
