2724ca6370d71e2147e55908a05e33f1d604df46
Echo Liang <echo.2.liang@gov.bc.ca>
2018-09-18 14:37:14 -0700

    config change

--- src/main/java/pssg/poc/justin/pocjivtd/PocjivtdApplication.java
+++ src/main/java/pssg/poc/justin/pocjivtd/PocjivtdApplication.java
@@ -1,7 +1,6 @@
 package pssg.poc.justin.pocjivtd;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableAutoConfiguration
 @ComponentScan(basePackages={"pssg.poc.justin.pocjivtd"})
 @EnableJpaRepositories(basePackages="pssg.poc.justin.pocjivtd.dataaccess.repository")
 @EnableTransactionManagement
