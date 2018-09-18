53da59af349795f73f9ef849e4edeae971826f75
Echo Liang <echo.2.liang@gov.bc.ca>
2018-09-18 14:07:03 -0700

    added @repository annotation

--- src/main/java/pssg/poc/justin/pocjivtd/dataaccess/repository/DisputeTicketRepository.java
+++ src/main/java/pssg/poc/justin/pocjivtd/dataaccess/repository/DisputeTicketRepository.java
@@ -3,6 +3,7 @@ package pssg.poc.justin.pocjivtd.dataaccess.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import pssg.poc.justin.pocjivtd.dataaccess.entity.DisputeTicketEntity;
 
@@ -10,6 +11,8 @@ import pssg.poc.justin.pocjivtd.dataaccess.entity.DisputeTicketEntity;
  * The Interface DisputeTicketRepository.
  * @author HLiang
  */
+
+@Repository
 public interface DisputeTicketRepository extends JpaRepository<DisputeTicketEntity, Long> {
 	
 	/**
@@ -18,10 +21,10 @@ public interface DisputeTicketRepository extends JpaRepository<DisputeTicketEnti
 	 * @param disputeID the dispute ID
 	 * @return the optional
 	 */
-	Optional<DisputeTicketEntity> findByDisputeId(Long disputeID);
+	//Optional<DisputeTicketEntity> findByDisputeId(Long disputeID);
 	
 	/* (non-Javadoc)
 	 * @see org.springframework.data.repository.CrudRepository#save(S)
 	 */
-	DisputeTicketEntity save(DisputeTicketEntity affiliationEntity);
+	//DisputeTicketEntity save(DisputeTicketEntity affiliationEntity);
 }
